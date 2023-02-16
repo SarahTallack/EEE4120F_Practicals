4ca755aa2c953663e21602de6ceeb59dffe74b0b
Sarah <sarahtallack@gmail.com>
2023-02-16 14:46:03 +0200

    Reformatted prac 1

--- Practical1.jl
+++ Practical1.jl
@@ -1,5 +1,4 @@
 #= 
-
 Practical 1
 
 Course: EEE4120F
@@ -7,30 +6,70 @@ Date: February 2023
 Authors:    
     Sarah Tallack       TLLSAR002
     Heather Wimberley   WMBHEA001
-
 =#
 
 using WAV
-using Dates
 using Plots
 using TickTock
 using Statistics
 
-# 1.2.1 Measuring Execution Time of rand()
-# t_start = now();
-whiteNoise = (rand(48000)*2).-1
 
-WAV.wavwrite(whiteNoise, "whiteNoise.wav", Fs = 4800) #sample freq is 48 kHz
-# t_end = now();
+function main()
+    # 1.2.1 Measuring Execution Time of rand()
+    tick()
 
-# execution = t_end - t_start;
-# println(t_start);
-# println(t_end);
-# println(execution);
+    whiteNoise = (rand(48000)*2).-1
+    WAV.wavwrite(whiteNoise, "whiteNoise.wav", Fs = 4800) #sample freq is 48 kHz
+    
+    tock()
+
+    # 1.2.2 White Noise Generator Script
+
+    tick()
+
+    whiteNoiseArray = createwhiten(10);
+    #print("Number of samples: ", size(whiteNoiseArray), "\n")
+    WAV.wavwrite(whiteNoiseArray,"white_noise_sound2.wav", Fs=4800)
+
+    tock()
+
+    # 1.2.3 Visual Confirmation of Uniform Distribution
+
+    h = Plots.histogram(whiteNoise,title = "Random Number Distribution",legend = false,xlabel = "Number Generated",ylabel = "Frequency")
+    Plots.display(h)
+
+    # 1.2.4 Timing Execution
+
+
+    # 1.2.6 Comparing Correlation Function to Statistics Package's Correlation Function
+    r1 = corr(whiteNoiseArray,whiteNoise)
+    println("Using corr function: ", r1);
+
+    r2 = cor(whiteNoiseArray,whiteNoise)
+    println("Using Statistics.cor function: ", r2, "\n");
+
+    # 1.2.7 Correlation of Shifted Signals
+
+    freq = [100, 1000, 48000];  # range of frequencies to test
+    Ns = [100, 1000, 10000];    # range of number of samples
+
+    for i in freq
+        for j in Ns
+            title = "Frequency of "* string(i)* "Hz, with "* string(j)* " samples"
+            Plots.display(Plots.scatter(sinusoid(i,j),title = title,legend = false,xlabel = "Time",ylabel = "Amplitude"))
+            sineWave = sinusoid(i,j);
+            sineWaveShift = circshift(sineWave, 10);
+            R = cor(sineWave,sineWaveShift);
+            println(title);
+            println("Correlation: ", R, "\n")
+        end
+    end
+
+end
 
 # 1.2.2 White Noise Generator Script
-
 function createwhiten(N)
+
     noiseArray = Array{Float64}(undef,N*4800)
     for i in N*4800
         noiseArray[i] = (rand()*2).-1
@@ -39,29 +78,7 @@ function createwhiten(N)
     return noiseArray
 end
 
-tick()
-
-whiteNoiseArray = createwhiten(10);
-
-print("Number of samples: ", size(whiteNoiseArray), "\n")
-WAV.wavwrite(whiteNoiseArray,"white_noise_sound2.wav", Fs=4800)
-
-tock()
-# whiten = createwhiten(1000); #create 1000 seconds of white noise
-# size(whiten);
-
-# 1.2.3 Visual Confirmation of Uniform Distribution
-
-h = Plots.histogram(whiteNoise,title = "Random Number Distribution",legend = false,xlabel = "Number Generated",ylabel = "Frequency")
-Plots.display(h)
-
-#readline() #this will stop the program at this point till you press enter
-
-# 1.2.4 Timing Execution
-
-
 # 1.2.5 Implementing Pearson's Correlation
-
 function corr(x,y)
     xavg = mean(x)
     yavg = mean(y)
@@ -69,13 +86,6 @@ function corr(x,y)
     return r
 end
     
-# 1.2.6 Comparing Correlation Function to Statistics Package's Correlation Function
-r1 = corr(whiteNoiseArray,whiteNoise)
-println("Using corr function: ", r1);
-
-r2 = cor(whiteNoiseArray,whiteNoise)
-println("Using Statistics.cor function: ", r2, "\n");
-
 # 1.2.7 Correlation of Shifted Signals
 
 function sinusoid(f, Ns)
@@ -86,17 +96,5 @@ function sinusoid(f, Ns)
     return wave
 end
 
-freq = [100, 1000, 48000];  # range of frequencies to test
-Ns = [100, 1000, 10000];    # range of number of samples
+main()
 
-for i in freq
-    for j in Ns
-        title = "Frequency of "* string(i)* "Hz, with "* string(j)* " samples"
-        Plots.display(Plots.scatter(sinusoid(i,j),title = title,legend = false,xlabel = "Time",ylabel = "Amplitude"))
-        sineWave = sinusoid(i,j);
-        sineWaveShift = circshift(sineWave, 10);
-        R = cor(sineWave,sineWaveShift);
-        println(title);
-        println("Correlation: ", R, "\n")
-    end
-end
