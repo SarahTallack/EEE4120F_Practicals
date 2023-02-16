96ffee518cde73cc86d0592209f21d2f5e6b5639
Sarah Tallack <sarahtallack@gmail.com>
2023-02-16 16:06:06 +0200

    Fixed the white noise

--- Practical1.jl
+++ Practical1.jl
@@ -19,7 +19,7 @@ function main()
     tick()
 
     whiteNoise = (rand(48000)*2).-1
-    WAV.wavwrite(whiteNoise, "whiteNoise.wav", Fs = 4800) #sample freq is 48 kHz
+    WAV.wavwrite(whiteNoise, "whiteNoise.wav", Fs = 48000); #sample freq is 48 kHz
     
     tock()
 
@@ -28,14 +28,16 @@ function main()
     tick()
 
     whiteNoiseArray = createwhiten(10);
-    #print("Number of samples: ", size(whiteNoiseArray), "\n")
-    WAV.wavwrite(whiteNoiseArray,"white_noise_sound2.wav", Fs=4800)
+    WAV.wavwrite(whiteNoiseArray, "white_noise_sound2.wav", Fs=4800);
 
     tock()
 
     # 1.2.3 Visual Confirmation of Uniform Distribution
 
     h = Plots.histogram(whiteNoise,title = "Random Number Distribution",legend = false,xlabel = "Number Generated",ylabel = "Frequency")
+    Plots.display(h)
+
+    h = Plots.histogram(whiteNoiseArray,title = "Other",legend = false,xlabel = "Number Generated",ylabel = "Frequency")
     Plots.display(h)
 
     # 1.2.4 Timing Execution
@@ -56,7 +58,7 @@ function main()
     for i in freq
         for j in Ns
             title = "Frequency of "* string(i)* "Hz, with "* string(j)* " samples"
-            Plots.display(Plots.scatter(sinusoid(i,j),title = title,legend = false,xlabel = "Time",ylabel = "Amplitude"))
+            #Plots.display(Plots.scatter(sinusoid(i,j),title = title,legend = false,xlabel = "Time",ylabel = "Amplitude"))
             sineWave = sinusoid(i,j);
             sineWaveShift = circshift(sineWave, 10);
             R = cor(sineWave,sineWaveShift);
@@ -69,12 +71,11 @@ end
 
 # 1.2.2 White Noise Generator Script
 function createwhiten(N)
-
     noiseArray = Array{Float64}(undef,N*4800)
-    for i in N*4800
+    for i in 1:N*4800
         noiseArray[i] = (rand()*2).-1
     end
-    WAV.wavwrite(noiseArray, "white_noise_sound1.wav", Fs = 4800) #sample freq is 48 kHz
+    WAV.wavwrite(noiseArray, "white_noise_sound1.wav", Fs = 4800) #sample freq is 4.8 kHz
     return noiseArray
 end
 
@@ -89,9 +90,12 @@ end
 # 1.2.7 Correlation of Shifted Signals
 
 function sinusoid(f, Ns)
+    # function to generate a single period of a sinusoid given its 
+    # frequency and the number of samples. 
+    # The sinusoid is stored in an array
     dt = 1/(Ns*f);
     t = 0:dt:1/f;
-    rad = 2*pi*f.*t
+    rad = 2*pi*f.*t;
     wave = sin.(rad);
     return wave
 end
