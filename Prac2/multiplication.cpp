3c7ca3b47928dd8263de73ca89a163a59714775a
Sarah Tallack <69241676+sjct00@users.noreply.github.com>
2023-03-28 14:43:18 +0200

    Update multiplication.cpp

--- Prac2/multiplication.cpp
+++ Prac2/multiplication.cpp
@@ -65,7 +65,7 @@ int main(void)
 
 	//New code for prac 2.2
 	bool displayMatrices = false;
-	int Size = 114;
+	int Size = 112;
 	int countA = Size*Size;
 	int matrixA[countA];
 	createKnownSquareMatrix(Size,matrixA, false);
