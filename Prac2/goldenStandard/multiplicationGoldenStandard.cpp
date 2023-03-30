99862e036f156f4f318c4799a4955ea51bab276c
Sarah <sarahtallack@gmail.com>
2023-03-30 11:15:36 +0200

    $1

--- Prac2/goldenStandard/multiplicationGoldenStandard.cpp
+++ Prac2/goldenStandard/multiplicationGoldenStandard.cpp
@@ -54,91 +54,93 @@ void createRandomSquareMatrix(int Size, int* squareMatrix, bool displayMatrices)
 
 int main(void){
 
+	int N[7] = {3, 8, 12, 16, 32, 64, 110};
+	for (int i = 0; i < 7; i++) {
 
 
-	//New code for prac 2.2
-	bool displayMatrices = true;
-	int Size = 3;
-	int countA = Size*Size;
-	int matrixA[countA];
-	createKnownSquareMatrix(Size,matrixA, false);
-	if(displayMatrices){
-		for(int i = 0; i<Size; i++){
-			for(int j = 0; j<Size; j++){
-					cout<<matrixA[i*Size+j]<<"\t ";
+		//New code for prac 2.2
+		bool displayMatrices = false;
+		int Size = N[i];
+		int countA = Size*Size;
+		int matrixA[countA];
+		createKnownSquareMatrix(Size,matrixA, false);
+		if(displayMatrices){
+			for(int i = 0; i<Size; i++){
+				for(int j = 0; j<Size; j++){
+						cout<<matrixA[i*Size+j]<<"\t ";
+					
+				}
+				
+				cout<<"\n";
 				
 			}
-			
-			cout<<"\n";
-			
 		}
-	}
-	cout<<"Number of elements in matrix 1: "<<countA<<"\n";
-	cout<<"Dimensions of matrix 1: "<<Size<<"x"<<Size<<"\n";
-	cout<<"Matrix 1 pointer: "<<matrixA<<"\n";
-	
+		cout<<"Number of elements in matrix 1: "<<countA<<"\n";
+		cout<<"Dimensions of matrix 1: "<<Size<<"x"<<Size<<"\n";
+		cout<<"Matrix 1 pointer: "<<matrixA<<"\n";
+		
 
-	
-	
-	int countB = Size*Size;
-	int matrixB[countB];
-	createKnownSquareMatrix(Size, matrixB, false);
-	for(int g = 0; g<countB; g++){
-		matrixB[g] = 2*matrixB[g];
-	}
-	if(displayMatrices){
-		for(int i = 0; i<Size; i++){
-			for(int j = 0; j<Size; j++){
-					cout<<matrixB[i*Size+j]<<"\t ";
+		
+		
+		int countB = Size*Size;
+		int matrixB[countB];
+		createKnownSquareMatrix(Size, matrixB, false);
+		for(int g = 0; g<countB; g++){
+			matrixB[g] = 2*matrixB[g];
+		}
+		if(displayMatrices){
+			for(int i = 0; i<Size; i++){
+				for(int j = 0; j<Size; j++){
+						cout<<matrixB[i*Size+j]<<"\t ";
+					
+				}
+				
+				cout<<"\n";
 				
 			}
-			
-			cout<<"\n";
-			
 		}
-	}
-	cout<<"Number of elements in matrix 2: "<<countB<<"\n";
-	cout<<"Dimensions of matrix 2: "<<Size<<"x"<<Size<<"\n";
-	cout<<"Matrix 2 pointer: "<<matrixB<<"\n";
-	
-	
-	start = clock(); //start running clock
+		cout<<"Number of elements in matrix 2: "<<countB<<"\n";
+		cout<<"Dimensions of matrix 2: "<<Size<<"x"<<Size<<"\n";
+		cout<<"Matrix 2 pointer: "<<matrixB<<"\n";
+		
+		
+		start = clock(); //start running clock
 
-	int outputAB[countA];
+		int outputAB[countA];
 
-	for (int i = 0; i < Size; i++) {//multiplies AB
-		for (int j = 0; j < Size; j++) {
-			outputAB[i*Size + j] = 0;
-			for (int k = 0; k < Size; k++) {
-				outputAB[i*Size + j] += matrixA[i*Size + k] * matrixB[j + k*Size];
-			}						
+		for (int i = 0; i < Size; i++) {//multiplies AB
+			for (int j = 0; j < Size; j++) {
+				outputAB[i*Size + j] = 0;
+				for (int k = 0; k < Size; k++) {
+					outputAB[i*Size + j] += matrixA[i*Size + k] * matrixB[j + k*Size];
+				}						
+			}
 		}
-	}
 
-	int output[countA];
+		int output[countA];
 
-	for (int i = 0; i < Size; i++) {//mutiplies the above matrix by A again
-		for (int j = 0; j < Size; j++) {
-			output[i*Size + j] = 0;
-			for (int k = 0; k < Size; k++) {
-				output[i*Size + j] += outputAB[i*Size + k] * matrixA[j + k*Size];
-			}						
+		for (int i = 0; i < Size; i++) {//mutiplies the above matrix by A again
+			for (int j = 0; j < Size; j++) {
+				output[i*Size + j] = 0;
+				for (int k = 0; k < Size; k++) {
+					output[i*Size + j] += outputAB[i*Size + k] * matrixA[j + k*Size];
+				}						
+			}
 		}
-	}
 
-	end = clock();//stop running clock
-	printf ("Run Time: %0.8f sec \n",((float) end - start)/CLOCKS_PER_SEC);
-	
-	//This if statement will display the matrix in output	
-	if(displayMatrices){
-		printf("\nOutput in the output_buffer \n");
-		for(int j=0; j<countA; j++) {
-			printf("%i \t " ,output[j]);
-			if(j%Size == (Size-1)){
-				printf("\n");
+		end = clock();//stop running clock
+		printf ("Run Time: %0.8f sec \n",((float) end - start)/CLOCKS_PER_SEC);
+		
+		//This if statement will display the matrix in output	
+		if(displayMatrices){
+			printf("\nOutput in the output_buffer \n");
+			for(int j=0; j<countA; j++) {
+				printf("%i \t " ,output[j]);
+				if(j%Size == (Size-1)){
+					printf("\n");
+				}
 			}
 		}
 	}
-	
 	return 0;
 }
